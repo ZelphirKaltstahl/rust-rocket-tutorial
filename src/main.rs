3920273c5a5b97b13067e9ac74bc51ab8fe169f0
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2017-06-21 01:39:09 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -54,95 +54,130 @@ mod json_handling {
 
 
 mod app_routes {
-    use app_structs::{Vocabulary, VocabularyContext};
-    use json_handling::{parse_from_file, encode_to_string};
+    use app_structs::{Vocabulary, VocabularyContext, Word};
+    use json_handling::encode_to_string;
+    use serde_json;
     use rocket::State;
+    use rocket::response::status::Status;
+    use rocket::response::content::JSON;
+    use rocket::response::Failure;
 
     #[get("/learn/<vocabulary_id>/<word_id>/<status>")]
     fn learn_word(vocabulary_id: &str, word_id: &str, status: bool) -> () {
         //
     }
-    #[get("/next_word/<vocabulary_id>")]
-    fn get_word(vocabulary_id: &str) -> String {
-        "Hello World!".to_string()
+
+    #[get("/word/<vocabulary_id>/<word_id>")]
+    fn get_word(vocabulary_context: State<VocabularyContext>,
+                vocabulary_id: &str,
+                word_id: &str) -> JSON<String> {
+        let the_vocabularies: Vec<Vocabulary> = vocabulary_context.vocabularies.iter()
+            .filter(|voc| voc.metadata.identifier == vocabulary_id)
+            .cloned()
+            .collect::<Vec<Vocabulary>>();
+
+        if the_vocabularies.is_empty() {
+            return Failure(Status::NotFound)
+        }
+
+        let the_words: Vec<Word> = the_vocabularies.first().unwrap().words.iter()
+            .filter(|word| word.metadata.id == word_id)
+            .cloned()
+            .collect::<Vec<Word>>();
+
+        if the_vocabularies.is_empty() {
+
+        }
+
+        let the_word_as_string: String = serde_json::to_string(&the_words[0])
+            .expect("could not serialize struct Vocabulary");
+
+        JSON(the_word_as_string)
+    }
+
+    #[get("/word/next/<vocabulary_id>")]
+    fn get_next_word(vocabulary_id: &str) -> () {
+        //
+    }
+    #[get("/word/random/<vocabulary_id>")]
+    fn get_random_word(vocabulary_id: &str) -> () {
+        //
     }
     #[get("/delete_word/<vocabulary_id>/<word_id>")]
     fn delete_word(vocabulary_id: &str, word_id: &str) -> () {
         //
     }
     #[get("/vocabulary/<vocabulary_id>")]
-    fn get_vocabulary(vocabulary_id: &str) -> String {
-        "My Vocabulary".to_string()
-    }
-
-    #[get("/get_me_a_json")]
-    fn get_me_a_json(vocabulary_context: State<VocabularyContext>) -> String {
-        let encoded_vocabulary: String = encode_to_string(&vocabulary_context.hsk1);
-        encoded_vocabulary
+    fn get_vocabulary(vocabulary_id: &str, vocabulary_context: State<VocabularyContext>) -> String {
+        let result: Vec<String> = vocabulary_context.vocabularies.iter()
+            .filter(|voc| voc.metadata.identifier.as_str().contains(vocabulary_id))
+            .map(encode_to_string)
+            .collect::<Vec<String>>();
+        result.join("\n\n")
     }
 }
 
 mod app_structs {
     use json_handling::parse_from_file;
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Clone)]
     pub struct Vocabulary {
-        metadata: VocabularyMetadata,
-        words: Vec<Word>
+        pub metadata: VocabularyMetadata,
+        pub words: Vec<Word>
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Clone)]
     pub struct VocabularyMetadata {
-        identifier: String,
-        learned_percentage: u8,
-        count: u32,
-        source_note: String
+        pub identifier: String,
+        pub learned_percentage: u8,
+        pub count: u32,
+        pub source_note: String
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Clone)]
     pub struct Word {
-        metadata: WordMetadata,
-        translation_data: WordTranslationData
+        pub metadata: WordMetadata,
+        pub translation_data: WordTranslationData
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Clone)]
     pub struct WordMetadata {
-        id: String,
-        learned: bool,
-        description: String
+        pub id: String,
+        pub learned: bool,
+        pub description: String
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Clone)]
     pub struct WordTranslationData {
-        english: String,
-        pinyin_numbered: String,
-        pinyin: String,
-        simplified: String,
-        traditional: String
+        pub english: String,
+        pub pinyin_numbered: String,
+        pub pinyin: String,
+        pub simplified: String,
+        pub traditional: String
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Clone)]
     pub struct VocabularyContext {
-        pub hsk1: Vocabulary,
-        pub hsk2: Vocabulary,
-        pub hsk3: Vocabulary,
-        pub hsk4: Vocabulary,
-        pub hsk5: Vocabulary,
-        pub hsk6: Vocabulary,
+        pub vocabularies: Vec<Vocabulary>
     }
 
     impl Default for VocabularyContext {
         fn default () -> VocabularyContext {
             VocabularyContext {
-                hsk1 : parse_from_file("data/hsk-1.json".to_string()),
-                hsk2 : parse_from_file("data/hsk-2.json".to_string()),
-                hsk3 : parse_from_file("data/hsk-3.json".to_string()),
-                hsk4 : parse_from_file("data/hsk-4.json".to_string()),
-                hsk5 : parse_from_file("data/hsk-5.json".to_string()),
-                hsk6 : parse_from_file("data/hsk-6.json".to_string()),
+                vocabularies : vec![
+                    parse_from_file("data/hsk-1.json".to_string()),
+                    parse_from_file("data/hsk-2.json".to_string()),
+                    parse_from_file("data/hsk-3.json".to_string()),
+                    parse_from_file("data/hsk-4.json".to_string()),
+                    parse_from_file("data/hsk-5.json".to_string()),
+                    parse_from_file("data/hsk-6.json".to_string())
+                ]
             }
         }
     }
+    // impl Clone for VocabularyContext {
+    //     fn clone(&self) -> VocabularyContext { *self }
+    // }
 }
 
 fn main() {
@@ -154,6 +189,5 @@ fn main() {
         .mount("/", routes![app_routes::get_word])
         .mount("/", routes![app_routes::delete_word])
         .mount("/", routes![app_routes::get_vocabulary])
-        .mount("/", routes![app_routes::get_me_a_json])
         .launch();
 }
